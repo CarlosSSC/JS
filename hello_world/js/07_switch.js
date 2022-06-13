db4f579cde8674b50f505314b483b6dbe0ed85c5
CarlosSSC <a6520150015@utch.edu.mx>
2022-06-13 08:33:47 -0600

    added 07_switch.js

---
+++ hello_world/js/07_switch.js
@@ -0,0 +1,20 @@
+var edad = 25
+var imprime = "";
+
+switch(edad){
+    case 18:
+        imprime = "Acabas de cumplir la mayoria de edad";
+    break;
+    case 25:
+        imprime = "ya eres un adulto";
+    break;
+    case 40:
+        imprime = "Crisis de los cuarenta";
+    break;
+    case 75:
+        imprime = "Ya eres un anciano";
+    break;
+    default:
+        imprime = "tu edad es neutral";
+        
+}
